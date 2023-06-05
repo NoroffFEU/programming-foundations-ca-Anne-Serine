8a0c2e4661567762ae9ec9ec20771b64662872e2
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2023-06-06 00:39:46 +0200

    added html, css and javascript

--- js/hamster.js
+++ js/hamster.js
@@ -7,9 +7,9 @@ const hamster = {
   yearOfBirth: 2022,
   favoriteFood: ["apple", "grapes", "cucumber", "melon"],
   respondsWhenYouWhistle: true,
-  whistle: function () {
+  eat: function () {
     console.log(
-      "Tripple X Baltazar looks out the door of his wooden house, and comes out to say hi"
+      "Stuffs as much food as he can in his mouth, so that he gets chubby cheeks"
     );
   },
   sleep: function () {
@@ -20,9 +20,38 @@ const hamster = {
       "Tripple X Baltazar runs in his exercise wheel, grooming and playing around"
     );
   },
-  eat: function () {
+  whistle: function () {
     console.log(
-      "Stuffs as much food as he can in his mouth, so that he gets chubby cheeks"
+      "Tripple X Baltazar looks out the door of his wooden house, and comes out to say hi"
     );
   },
+};
+
+var button1 = document.querySelector(".button1");
+var button2 = document.querySelector(".button2");
+var button3 = document.querySelector(".button3");
+var button4 = document.querySelector(".button4");
+
+console.dir(button1);
+
+button1.onclick = function () {
+  console.log(
+    "Stuffs as much food as he can in his mouth, so that he gets chubby cheeks"
+  );
+};
+
+button2.onclick = function () {
+  console.log("Resting to be ready for an active night");
+};
+
+button3.onclick = function () {
+  console.log(
+    "Tripple X Baltazar runs in his exercise wheel, grooming and playing around"
+  );
+};
+
+button4.onclick = function () {
+  console.log(
+    "Tripple X Baltazar looks out the door of his wooden house, and comes out to say hi"
+  );
 };
