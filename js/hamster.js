8f158e25e3c3ac55920b4156929a52b8d51d207d
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2023-06-06 11:22:11 +0200

    added function to button.onclick

--- js/hamster.js
+++ js/hamster.js
@@ -27,31 +27,48 @@ const hamster = {
   },
 };
 
-var button1 = document.querySelector(".button1");
-var button2 = document.querySelector(".button2");
-var button3 = document.querySelector(".button3");
-var button4 = document.querySelector(".button4");
+var heading = document.querySelector("h1");
+var subHeading = document.querySelector("h2");
+var button1 = document.querySelector("#button1");
+var button2 = document.querySelector("#button2");
+var button3 = document.querySelector("#button3");
+var button4 = document.querySelector("#button4");
+var output = document.querySelector("#output");
 
-console.dir(button1);
 
-button1.onclick = function () {
-  console.log(
-    "Stuffs as much food as he can in his mouth, so that he gets chubby cheeks"
-  );
-};
+heading.innerHTML = ("Trippel X Baltazar");
+subHeading.innerHTML = ("- the dwarf hamster -");
+button1.innerHTML = "Eat";
+button2.innerHTML = "Sleep";
+button3.innerHTML = "Awake";
+button4.innerHTML = "Whistle";
 
-button2.onclick = function () {
-  console.log("Resting to be ready for an active night");
-};
 
-button3.onclick = function () {
-  console.log(
-    "Tripple X Baltazar runs in his exercise wheel, grooming and playing around"
-  );
-};
+function eat() {
+  output.innerHTML = "Stuffs as much food as he can in his mouth, so that he gets chubby cheeks";
+}
 
-button4.onclick = function () {
-  console.log(
-    "Tripple X Baltazar looks out the door of his wooden house, and comes out to say hi"
-  );
-};
+button1.onclick = eat;
+
+
+function sleep() {
+  output.innerHTML = "Resting to be ready for an active night";
+}
+
+button2.onclick = sleep;
+
+
+function awake() {
+  output.innerHTML = "Tripple X Baltazar runs in his exercise wheel, grooming and playing around";
+}
+
+button3.onclick = awake;
+
+
+function whistle() {
+  output.innerHTML = "Tripple X Baltazar looks out the door of his wooden house, and comes out to say hi"
+}
+
+button4.onclick = whistle;
+
+
